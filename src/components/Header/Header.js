14a4be948d0f96ae731e6110abcea788ac4d327d
Chris Anderson <mistercanderson@gmail.com>
2021-07-31 15:13:13 -0600

    Add functionality to header buttons & add social media links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,7 @@
 import './Header.css';
-import PhoneIcon from '../../assets/icons/phone-call.svg';
-import EmailIcon from '../../assets/icons/email.svg';
 import HeaderButton from './HeaderButton/HeaderButton';
 import Logo from '../Logo/Logo';
+import { email } from '../../util/email';
 
 export default function Header({ displayContactForm, screenWidth }) {
   const scrollToElement = (e) => {
@@ -18,21 +17,54 @@ export default function Header({ displayContactForm, screenWidth }) {
   return (
     <header>
       <Logo disabled={false} />
-      {/* {screenWidth > 480 && ( */}
-        <nav className='page-buttons'>
-          <div className='page-buttons-list'>
-            {/* <HeaderButton text='Home' /> */}
-            <HeaderButton text='Contact Us' handleClick={displayContactForm} />
-            <HeaderButton text='Services' handleClick={scrollToElement} />
-            <HeaderButton text='Testimonials' handleClick={scrollToElement} />
-            <HeaderButton text='Who We Are' handleClick={scrollToElement} />
-          </div>
-        </nav>
-      {/* )} */}
+      <nav className='page-buttons'>
+        <div className='page-buttons-list'>
+          <HeaderButton text='Contact Us' handleClick={displayContactForm} />
+          <HeaderButton text='Services' handleClick={scrollToElement} />
+          <HeaderButton text='Testimonials' handleClick={scrollToElement} />
+          <HeaderButton text='Who We Are' handleClick={scrollToElement} />
+        </div>
+      </nav>
       <div className='contact-info-container'>
         <div className='contact-info'>
-          <HeaderButton text='(720) 380-4763' icon={PhoneIcon} />
-          <HeaderButton text='GatesEnterprisesLLC@gmail.com' icon={EmailIcon} />
+          {screenWidth > 480 && (
+            <>
+              <a href='tel:17207758369'>
+                <HeaderButton name='phone' text='(720) 380-4763' icon={true} />
+              </a>
+              <a
+                href={`mailto:${email.address}?&subject=${email.subject}&body=${email.body}`}
+              >
+                <HeaderButton
+                  name='email'
+                  text='GatesEnterprisesLLC@gmail.com'
+                  icon={true}
+                />
+              </a>
+              <HeaderButton name='instagram' icon={true} />
+              <HeaderButton name='facebook' icon={true} />
+            </>
+          )}
+          {screenWidth <= 480 && (
+            <>
+              <a
+                href={`mailto:${email.address}?&subject=${email.subject}&body=${email.body}`}
+              >
+                <HeaderButton
+                  name='email'
+                  text='GatesEnterprisesLLC@gmail.com'
+                  icon={true}
+                />
+              </a>
+              <a href='tel:17207758369'>
+                <HeaderButton name='phone' text='(720) 380-4763' icon={true} />
+              </a>
+              <div className='social-media-container'>
+                <HeaderButton name='instagram' icon={true} />
+                <HeaderButton name='facebook' icon={true} />
+              </div>
+            </>
+          )}
         </div>
       </div>
     </header>
---
+++ src/util/email.js
@@ -0,0 +1,5 @@
+export const email = {
+  address: 'fakeemail@poop.com',
+  subject: 'Free Inspection',
+  body: 'Hi I demand a free inspection NOW',
+};
