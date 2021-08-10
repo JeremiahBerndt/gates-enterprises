8a23483b68ce03af0678e25f41c2b50b0b19f848
Jeremiah <JeremiahBerndt@gmail.com>
2021-08-10 13:35:27 -0600

    testing netlify again

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from '../Header/Header';
 import MainPage from '../MainPage/MainPage';
-import Footer from '../Footer/Footer';
 import ContactForm from '../ContactForm/ContactForm';
 import Services from '../MainPage/Services/Services';
 import WhoWeAre from '../MainPage/WhoWeAre/WhoWeAre';
@@ -37,7 +36,6 @@ function App() {
         <Route exact path='/financing' render={() => <Financing name='Financing'/>} />
         <Route exact path='/partners' render={() => <Partners name='Partners'/>} />
       </Switch>
-      {/* <Footer /> */}
       <ContactForm
         displayContactForm={displayContactForm}
         openContact={openContact}
--- src/components/Footer/Footer.js
+++
@@ -1,5 +0,0 @@
-import './Footer.css';
-
-export default function Footer() {
-  return <footer>Hi I'm a footer</footer>;
-}
