d8cd6a16b75fcfe9ec76a8f912f6947a39733dea
Jeremiah Berndt <jeremiah@Jeremiahs-MacBook-Pro.local>
2023-07-12 14:29:04 -0600

    netlify toml

--- vite.config.js
+++ vite.config.js
@@ -10,12 +10,6 @@ export default defineConfig(() => {
     build: {
       outDir: 'build',
     },
-    plugins: [react({
-      jsxImportSource: '@emotion/react',
-        // babel: {
-        //   plugins: ['@emotion/babel-plugin'],
-        // },
-      }
-    ), svgrPlugin()],
+    plugins: [react({ jsxImportSource: '@emotion/react' }), svgrPlugin()],
   };
 });
