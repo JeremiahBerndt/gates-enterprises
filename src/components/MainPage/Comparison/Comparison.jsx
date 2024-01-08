4f7f67942f03e6605087455b306a39e070400d95
Jeremiah Berndt <jeremiah@Jeremiahs-MacBook-Pro.local>
2024-01-08 16:19:55 -0700

    trying to fix safari video

--- src/components/MainPage/Comparison/Comparison.jsx
+++ src/components/MainPage/Comparison/Comparison.jsx
@@ -8,7 +8,7 @@ const Comparison = () => {
 
   return (
     <>
-      <video autoPlay loop muted playsinline style={{
+      <video autoplay loop muted playsinline style={{
         position: "absolute",
         top: 0,
         left: 0,
